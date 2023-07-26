98455faea4c7d923839d1c984358138ec1a1b725
Gabriel <andradeg2015@gmail.com>
2023-07-26 13:30:25 -0300

    front-end changes

--- public/index.js
+++ public/index.js
@@ -1,7 +1,40 @@
+document.addEventListener('DOMContentLoaded', () => {
+    showUsers();
+})
+
 const nameInput = document.getElementById("name-input");
 const emailInput = document.getElementById("email-input");
 const phoneInput = document.getElementById("phone-input");
 const birthInput = document.getElementById("birth-input");
+
+const render = async () => {
+    // getUsers();
+}
+
+const showUsers = async () => {
+    try {
+        const data = await fetch("http://localhost:3030/api/all");
+        const obj = await data.json();
+        console.log(obj);
+
+        let usersElements = '';
+
+        obj.forEach(user => {
+            let userElement = `<div class="users-obj" id=${user.id}>
+                <span class="name-list">${user.name}</span>
+                <span class="mail-list">${user.email}</span>
+                <span class="phone-list">${user.phone}</span>
+             </div>`
+
+             usersElements += userElement;
+        });
+
+        document.querySelector('.users-list').innerHTML = usersElements;
+
+    } catch (error) {
+        console.log(error)
+    }
+}
 
 
 const saveUser = async () => {
@@ -18,8 +51,6 @@ const saveUser = async () => {
             phone,
             birthDay
         }
-
-        console.log(JSON.stringify(birthDay));
 
         const options = {method:"POST",
         headers: new Headers({'content-type': 'application/json'}),
